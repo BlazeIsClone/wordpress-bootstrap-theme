1aacc95e0c73302558ad40dce7c8745e22a544f4
BlazeIsClone <64116737+BlazeIsClone@users.noreply.github.com>
2022-04-12 22:56:33 +0530

    add 404 page

---
+++ theme-base/404.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * The template for not found page
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ */
+get_header(); ?>
+
+<h1><?php _e('Page Not Found'); ?>
+</h1>
+
+<?php get_footer();
